2dced452cd034997a7127118b482729940883ede
justincory <iamjustincory@gmail.com>
2023-03-05 19:09:10 -0800

    Lots of fixes and updates to hank's

---
+++ hanks/script.js
@@ -0,0 +1,12 @@
+var video = document.getElementById("video");
+var btn = document.getElementByClass("secondary-button");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}

57dd20489410d1494131b3d51fe694ea5a58da52
justincory <iamjustincory@gmail.com>
2023-03-08 11:09:03 -0800

    marquee

--- hanks/script.js
+++ hanks/script.js
@@ -10,21 +10,36 @@ function myFunction() {
     btn.innerHTML = "Play";
   }
 }
+
 'use strict'
 
+
 function toggleIcon() {
-  $('.burger').on('click', function () {
-    $('.burger').toggleClass('active')
-    $('.menu-mobile').slideToggle(300)
-  })
+    $('.icon').on('click', function(){
+        $('.icon').toggleClass('active');
+        $('.menu-mobile').slideToggle(300);
+    })
 }
 
-function closeMobileMenu() {
-  $('.menu-mobile').on('click', 'a', function () {
-    $('.burger').trigger('click')
-  })
+function closeMobileMenu(){
+    $('.menu-mobile').on('click', 'a', function(){
+        $('.icon').trigger('click');
+    })
+}
+
+
+
+function desktopMenu() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50 && $(window).width() > 768) {
+            $('header.desktop').fadeIn(500);
+        } else {
+            $('header.desktop').fadeOut(500);
+        }
+    })
 }
 
 //when the page loads call toggleIcon;
-$(toggleIcon)
-$(closeMobileMenu)
+$(toggleIcon);
+$(closeMobileMenu);
+$(desktopMenu);

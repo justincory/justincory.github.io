5cbb86231c7cccb15195ffb1456640a74c7e91a1
alannarisse <angrypirate@gmail.com>
2023-03-09 14:07:34 -0800

    fixed pause play and nav

--- hanks/script.js
+++ hanks/script.js
@@ -1,42 +1,40 @@
-var video = document.getElementById("video");
-var btn = document.getElementByClass("secondary-button");
+'use strict'
+
+var video = document.getElementById('video')
+var btn = document.getElementById('pause-button')
+var btnTxt = document.getElementById('button-text')
 
 function myFunction() {
   if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
+    video.play()
+    btnTxt.innerHTML = 'Pause'
   } else {
-    video.pause();
-    btn.innerHTML = "Play";
+    video.pause()
+    btnTxt.innerHTML = 'Play'
   }
 }
 
-'use strict'
-
-
 function toggleIcon() {
-    $('.icon').on('click', function(){
-        $('.icon').toggleClass('active');
-        $('.menu-mobile').slideToggle(300);
-    })
+  $('.icon').on('click', function () {
+    $('.icon').toggleClass('active')
+    $('.menu-mobile').slideToggle(300)
+  })
 }
 
-function closeMobileMenu(){
-    $('.menu-mobile').on('click', 'a', function(){
-        $('.icon').trigger('click');
-    })
+function closeMobileMenu() {
+  $('.menu-mobile').on('click', 'a', function () {
+    $('.icon').trigger('click')
+  })
 }
-
-
 
 function desktopMenu() {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 50 && $(window).width() > 768) {
-            $('header.desktop').fadeIn(500);
-        } else {
-            $('header.desktop').fadeOut(500);
-        }
-    })
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50 && $(window).width() > 768) {
+      $('header.desktop').fadeIn(500)
+    } else {
+      $('header.desktop').fadeOut(500)
+    }
+  })
 }
 $('.effects').click(function () {
   $('.fxcat').css('display', 'block')
@@ -63,37 +61,36 @@ $('.accessories').click(function () {
   $('.shopicons').not('.accessoriescat').css('display', 'none')
 })
 $(function () {
-  var img = $('.knob');
+  var img = $('.knob')
 
   if (img.length > 0) {
-    var offset = img.offset();
+    var offset = img.offset()
 
     $('.animation-trigger').mouseenter(function (event) {
-      var element = $(this);
-      var elementPosition = element.offset();
-      var elementX = elementPosition.left + (element.width() / 2);
-      var elementY = elementPosition.top + (element.height() / 2);
-      var imgX = offset.left + (img.width() / 2);
-      var imgY = offset.top + (img.height() / 2);
-      var radians = Math.atan2(elementX - imgX, elementY - imgY);
-      var degrees = (radians * (180 / Math.PI) * -1) + 180;
-      if(degrees <= 100){
-        degrees = degrees-35
+      var element = $(this)
+      var elementPosition = element.offset()
+      var elementX = elementPosition.left + element.width() / 2
+      var elementY = elementPosition.top + element.height() / 2
+      var imgX = offset.left + img.width() / 2
+      var imgY = offset.top + img.height() / 2
+      var radians = Math.atan2(elementX - imgX, elementY - imgY)
+      var degrees = radians * (180 / Math.PI) * -1 + 180
+      if (degrees <= 100) {
+        degrees = degrees - 35
       } else {
-        degrees = degrees+10
+        degrees = degrees + 10
       }
 
-      img.css('-moz-transform', 'rotate(' + degrees + 'deg)')
+      img
+        .css('-moz-transform', 'rotate(' + degrees + 'deg)')
         .css('-webkit-transform', 'rotate(' + degrees + 'deg)')
         .css('-o-transform', 'rotate(' + degrees + 'deg)')
-        .css('-ms-transform', 'rotate(' + degrees + 'deg)');
-        //alert(degrees)
-    });
+        .css('-ms-transform', 'rotate(' + degrees + 'deg)')
+      //alert(degrees)
+    })
   }
-});
+})
 //when the page loads call toggleIcon;
-$(toggleIcon);
-$(closeMobileMenu);
-$(desktopMenu);
-
-
+$(toggleIcon)
+$(closeMobileMenu)
+$(desktopMenu)
